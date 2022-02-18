a6bff5d4d2791f39d6860c40cbfad1bbf6f6e9b4
Antonio Stasio <astasio@gmail.com>
2022-02-18 11:01:14 +0100

    updated translations, removed copyright images, add recharge SIM function

--- app/src/main/java/com/astasio/siminfo/MainActivity2.java
+++ app/src/main/java/com/astasio/siminfo/MainActivity2.java
@@ -1,10 +1,8 @@
 package com.astasio.siminfo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -12,9 +10,5 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Button button = findViewById(R.id.button_back);
-        button.setOnClickListener(
-                v -> startActivity(new Intent(MainActivity2.this,
-                        MainActivity.class)));
     }
 }
